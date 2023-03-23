16c67e49766ad76b36c139c5c9d5f459e90c80c7
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-03-23 18:05:52 +0800

    Update video handler

--- src/api/videos/video.handler.js
+++ src/api/videos/video.handler.js
@@ -4,13 +4,12 @@ import * as videos from "./videos.js";
 export async function addVideo(req, res)
 {
     try{
-        const videoId = await videos.addVideo(req.body);
-        console.log('videoId: ', videoId);
+        const videoId = await videos.addVideo(req.query);
         res.status(200).send(videoId);
     }
     catch(e)
     {
-        res.status(500).send(res.message);
+        res.status(500).send(e.message);
     }
 }
 

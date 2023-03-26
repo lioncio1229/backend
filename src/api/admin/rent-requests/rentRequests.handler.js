4b18af4f4057845326e03d7284d1155e4a91b5fe
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-03-26 15:04:57 +0800

    Fix

--- src/api/admin/rent-requests/rentRequests.handler.js
+++ src/api/admin/rent-requests/rentRequests.handler.js
@@ -11,7 +11,7 @@ async function acceptRentRequest(req, res)
             return;
         }
 
-        const result = await rents.addRent(req.username, rent.videoId);
+        const result = await rents.addRent(rent.username, rent.videoId);
         await rentRequests.deleteRentRequest(rentRequestId);
         res.status(200).send(result);
     }
